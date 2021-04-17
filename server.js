23cd0b741912170f52b020ebc8665ce08acf7c19
ldylicia <fharrisfoster@gmail.com>
2021-04-16 20:30:56 -0700

    add server.js file

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const path=require ("path")
-const express=require("express")
+const path = require("path")
+const express = require("express")
 
-const app=express()
-const PORT=process.env.PORT || 3001
+const app = express()
+const PORT = process.env.PORT || 3001
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(express.static("public"))
 
 
-app.listen(PORT,function(){
+app.listen(PORT, function () {
     console.log("app listening on port " + PORT)
 })
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./public/index.html"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 
 })
