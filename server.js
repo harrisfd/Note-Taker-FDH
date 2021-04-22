102843b75bc4286477a44ad8a6162d1683cbb6cf
ldylicia <fharrisfoster@gmail.com>
2021-04-21 20:24:12 -0700

    update server.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const express = require("express")
+const fs= require("fs")
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -17,4 +18,24 @@ app.listen(PORT, function () {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 
+})
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+
+})
+
+app.get("/api/notes", (req,res)=>{
+    const data=fs.readFileSync(path.join(__dirname,"./db/db.json"), "utf8")
+    const parsdata=JSON.parse(data)
+    
+    
+    res.json(parsdata)
+})
+
+app.post("/api/notes", (req,res)=>{
+    const data=fs.readFileSync(path.join(__dirname,"./db/db.json"), "utf8")
+    const parsdata=JSON.parse(data)
+    parsdata.push(req.body)
+   fs.writeFileSync(path.join(__dirname, "./db/db.json"),JSON.stringify(parsdata))
+   res.json(parsdata)
 })
