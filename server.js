1d9976bf1346eebff88c93f92f87baef86a6cfdd
ldylicia <fharrisfoster@gmail.com>
2021-04-25 18:48:35 -0700

    updated deletion command and add ids

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const express = require("express")
 const fs= require("fs")
+const shortid=require("shortid")
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -35,7 +36,24 @@ app.get("/api/notes", (req,res)=>{
 app.post("/api/notes", (req,res)=>{
     const data=fs.readFileSync(path.join(__dirname,"./db/db.json"), "utf8")
     const parsdata=JSON.parse(data)
+
+    const id=shortid.generate()
+    req.body.id=id
     parsdata.push(req.body)
    fs.writeFileSync(path.join(__dirname, "./db/db.json"),JSON.stringify(parsdata))
    res.json(parsdata)
+})
+
+app.delete("/api/notes/:id",(req,res)=>{
+    const id=req.params.id
+    const data=fs.readFileSync(path.join(__dirname,"./db/db.json"), "utf8")
+    const parsdata=JSON.parse(data)
+   const index=parsdata.findIndex(element =>{
+       if(id===element.id){
+           return true
+       }
+   }) 
+   parsdata.splice(index,1)
+   fs.writeFileSync(path.join(__dirname, "./db/db.json"),JSON.stringify(parsdata))
+   res.json()
 })
